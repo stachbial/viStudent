755100a83b19a3e4e9c482642d7c4befa936511d
stachbial <stasiu.bialecki@gmail.com>
2022-10-10 00:13:07 +0200

    test rust <-> next js image data exchange and display possibilities

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,35 +7,36 @@ use opencv::{
     core,
     highgui,
     prelude::*,
-    videoio,
+    videoio, imgcodecs::IMREAD_UNCHANGED,
 };
+
 
 // use core::str::Bytes;
 
-fn runopencvexample() -> opencv::Result<()> {
-    let window = "video capture";
-    highgui::named_window(window, 1)?;
-    #[cfg(feature = "opencv-32")]
-    let mut cam = videoio::VideoCapture::new_default(0)?;  // 0 is the default camera
-    #[cfg(not(feature = "opencv-32"))]
-    let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;  // 0 is the default camera
-    let opened = videoio::VideoCapture::is_opened(&cam)?;
-    if !opened {
-        panic!("Unable to open default camera!");
-    }
-    loop {
-        let mut frame = core::Mat::default();
-        cam.read(&mut frame)?;
-        if frame.size()?.width > 0 {
-            highgui::imshow(window, &mut frame)?;
-        }
-        let key = highgui::wait_key(10)?;
-        if key > 0 && key != 255 {
-            break;
-        }
-    }
-    Ok(())
-}
+// fn runopencvexample() -> opencv::Result<()> {
+//     let window = "video capture";
+//     highgui::named_window(window, 1)?;
+//     #[cfg(feature = "opencv-32")]
+//     let mut cam = videoio::VideoCapture::new_default(0)?;  // 0 is the default camera
+//     #[cfg(not(feature = "opencv-32"))]
+//     let mut cam = videoio::VideoCapture::new(0, videoio::CAP_ANY)?;  // 0 is the default camera
+//     let opened = videoio::VideoCapture::is_opened(&cam)?;
+//     if !opened {
+//         panic!("Unable to open default camera!");
+//     }
+//     loop {
+//         let mut frame = core::Mat::default();
+//         cam.read(&mut frame)?;
+//         if frame.size()?.width > 0 {
+//             highgui::imshow(window, &mut frame)?;
+//         }
+//         let key = highgui::wait_key(10)?;
+//         if key > 0 && key != 255 {
+//             break;
+//         }
+//     }
+//     Ok(())
+// }
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
@@ -44,17 +45,40 @@ fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-// #[tauri::command]
-// fn test(img: &Arc<u8>, lenght: &u8) -> Arc<u8> {
-//     // runopencv().unwrap();
 
+fn convert_str_to_u8vector(str: &str) -> Vec<u8> {
+    // runopencvexample().unwrap();
+    let split = str.split(",");
+    let string_vec = split.collect::<Vec<&str>>();
+    let mut u8vec = Vec::new();
 
-//     format!("Hello, {}! You've been greeted from Rust!")
-// }
+    for i in string_vec {
+        let el = i.parse::<u8>().unwrap();
+        u8vec.push(el)
+    }
+
+    return u8vec;
+}
+
+#[tauri::command]
+fn test(img: &str) -> String {
+    let u8vec = convert_str_to_u8vector(img);
+
+    
+    let vector = opencv::types::VectorOfu8::from_iter(u8vec);
+    let mat = opencv::imgcodecs::imdecode(&vector, IMREAD_UNCHANGED).unwrap();
+
+    let mut buffer = opencv::types::VectorOfu8::new();
+    let params =  opencv::types::VectorOfi32::new();
+    let _test = opencv::imgcodecs::imencode(".jpg", &mat, &mut buffer, &params ).unwrap();
+    let output  = buffer.as_slice();
+
+    format!("{:?}", output)
+}
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/modules/media/MediaInput.tsx
+++ src/modules/media/MediaInput.tsx
@@ -11,35 +11,36 @@ import AttachFileOutlinedIcon from "@mui/icons-material/AttachFileOutlined";
 import ImageSearchOutlinedIcon from "@mui/icons-material/ImageSearchOutlined";
 import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
 
-import { invoke } from "@tauri-apps/api";
+import { invoke } from "@tauri-apps/api/tauri";
 
 const MediaInput = () => {
   const [uploadedImageData, setUploadedImageData] = useState<string>();
+
+  const testRustInvoke = async (params: { img: string }) => {
+    const msgFromRust = await invoke("test", params);
+    return msgFromRust as string;
+  };
 
   const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
     event.stopPropagation();
     event.preventDefault();
 
-    const test = 8;
-    const u8test = new Uint8Array(test);
-    u8test[0] = test;
-    console.log(test.toString());
-
     if (event.target.files && event.target.files[0]) {
-      const test = event.target.files[0];
-      // let imageUrl = window.URL.createObjectURL(test);
-      // setUploadedImageData(imageUrl);
-
       const reader = new FileReader();
       reader.readAsArrayBuffer(event.target.files[0]);
 
-      console.log(reader);
-
-      // const data = new Uint8Array(ev.target.result);
-      reader.onload = (ev) => {
+      reader.onload = async (ev) => {
         const u8data = new Uint8Array(ev.target.result as ArrayBuffer);
-        const blob = new Blob([u8data]);
 
+        const rustString = await testRustInvoke({ img: u8data.toString() });
+        console.log(rustString);
+        const arrayFromRustStr = rustString
+          .replace("[", "")
+          .replace("]", "")
+          .split(",")
+          .map((el) => parseInt(el));
+        const u8fromRustStr = new Uint8Array(arrayFromRustStr);
+        const blob = new Blob([u8fromRustStr]);
         let imageUrl = window.URL.createObjectURL(blob);
         setUploadedImageData(imageUrl);
       };
@@ -49,12 +50,14 @@ const MediaInput = () => {
   return (
     <StyledModuleWrapper item sm>
       <StyledContentWrapper>
-        <Image
-          width={300}
-          height={300}
-          objectFit="cover"
-          src={uploadedImageData}
-        />
+        {uploadedImageData && (
+          <Image
+            width={300}
+            height={300}
+            objectFit="cover"
+            src={uploadedImageData}
+          />
+        )}
         <AttachFileOutlinedIcon sx={{ fontSize: 80 }} />
         <StyledInputControlsWrapper>
           <Button
