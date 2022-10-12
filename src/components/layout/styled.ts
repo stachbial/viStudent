cabd2c51a9376737c4017cc605338350f5c00444
stachbial <stasiu.bialecki@gmail.com>
2022-10-12 21:36:30 +0200

    add background

--- src/components/layout/styled.ts
+++ src/components/layout/styled.ts
@@ -1,7 +1,18 @@
 import styled from "@emotion/styled";
 import Grid from "@mui/material/Grid";
-
+import bg from "../../assets/bg.png";
 export const StyledContainer = styled(Grid)`
   width: 100%;
   height: 100vh;
+
+  &::before {
+    position: absolute;
+    content: "";
+    width: 100%;
+    height: 100%;
+    background-image: url(${bg.src});
+    z-index: -999;
+    filter: invert(1);
+    opacity: 0.03;
+  }
 `;
